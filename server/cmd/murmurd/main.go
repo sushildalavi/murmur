24990a2538602aa5c87c0b13e40750db8a1d1f13
sushildalavi <75267758+sushildalavi@users.noreply.github.com>
2026-06-02 20:43:39 -0700

    Add the server entry point

---
+++ server/cmd/murmurd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ok"))
+	})
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Fatal(server.ListenAndServe())
+}
